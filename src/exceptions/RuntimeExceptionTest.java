23fa3076aa1f73369fb6105cadad2d4156ed40fd
stormaroon <stormaroon@gmail.com>
2016-11-24 10:51:30 +0800

    修改二叉树实现

---
+++ src/exceptions/RuntimeExceptionTest.java
@@ -0,0 +1,26 @@
+package exceptions;
+
+import java.util.NoSuchElementException;
+
+/**
+ * Created by llwxg on 2016/11/23.
+ */
+public class RuntimeExceptionTest {
+    private void runExceptionMaker() {
+        throw new RuntimeException("this is a runtime exception");
+    }
+
+    public void catchTest() {
+        try {
+            this.runExceptionMaker();
+        } catch (RuntimeException e) {
+            System.out.println("can not catch a runtime exception");
+        }
+    }
+
+    public static void main(String[] args) {
+        RuntimeExceptionTest test = new RuntimeExceptionTest();
+        test.catchTest();
+    }
+}
+
